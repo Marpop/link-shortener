65a3ea191a9aae4ece052af9bed436dae38cc916
Marcin Popławski <marpop8@gmail.com>
2022-08-21 23:44:21 +0200

    Fix mypy urls errors

--- config/api_router.py
+++ config/api_router.py
@@ -1,14 +1,6 @@
-from django.conf import settings
+from rest_framework.routers import DefaultRouter
 
-from rest_framework.routers import DefaultRouter, SimpleRouter
-
-if settings.DEBUG:
-    router = DefaultRouter()
-else:
-    router = SimpleRouter()
-
-#
-
+router = DefaultRouter()
 
 app_name = "api"
 urlpatterns = router.urls
